e6b4c1c22b6a59c6e20179ca9d72f7c66f33766f
juan-suarez <jsuarez0698@gmail.com>
2022-07-26 16:55:02 -0400

    searchbar almost done

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -9,7 +9,8 @@ const Gf = () => {
         const fetchingData = async () =>{
             const result = await axios("https://api.giphy.com/v1/gifs/trending", {
                 params: {
-                    api_key:"ESXpJv9GdToCoypY0MQTzMNJ56DLaKu6"
+                    api_key:"ESXpJv9GdToCoypY0MQTzMNJ56DLaKu6",
+                    limit:20
                 }
             });
             console.log(result);
--- src/components/GIF-app-searchbar.jsx
+++ src/components/GIF-app-searchbar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import  {BiSearchAlt} from "react-icons/bi";
+import axios from "axios";
 
 const Write = () =>{
+    const[search, setSearch] = useState("");
+    const[info, setInfo] = useState();
+    const changeQuest = (e)=>{
+        setSearch(e.target.value)
+    };
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const fetchData = async() => {
+        const catchData = await axios("https://api.giphy.com/v1/gifs/search",{
+            params:{
+                api_key:"ESXpJv9GdToCoypY0MQTzMNJ56DLaKu6",
+                q:search
+            }
+        });
+        console.log(catchData)
+        setInfo(catchData.data.data)
+    };
+    fetchData();
+}
+
     return(
         <div className='placeholder'>
       <form className='forms'> 
@@ -10,9 +32,11 @@ const Write = () =>{
          autoCorrect='off' 
          autoComplete='off'
          placeholder='Search GIFs here'
-         className='writting'>
+         className='writting'
+         onChange={changeQuest}
+         value={search}>
          </input>
-         <button type="submit" className="btn">
+         <button type="submit" onClick={handleSubmit} className="btn">
             <BiSearchAlt/>
             </button>
         </form> 
