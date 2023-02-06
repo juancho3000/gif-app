765f130126dcbd66424925986d160b06fcf54b83
juan-suarez <jsuarez0698@gmail.com>
2023-02-06 10:36:03 -0400

    fixing styles and tags

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -4,20 +4,20 @@ const Tags = () =>{
     return(
         <div className='menu-choices'>
         <ul className='menu-containers'>
-          <li className='menu-list1 YRN'>
-            <a href='https://giphy.com/'>Reactions</a>
+          <li className='menu-list1 TAG'>
+            <a href='https://giphy.com/reactions'>Reactions</a>
           </li>
-          <li className='menu-list2 YRN'>
-          <a href='https://giphy.com/'>Entertainment</a>
+          <li className='menu-list2 TAG'>
+          <a href='https://giphy.com/entertainment'>Entertainment</a>
           </li>
-          <li className='menu-list3 YRN'>
-          <a href='https://giphy.com/'>Sports</a>
+          <li className='menu-list3 TAG'>
+          <a href='https://giphy.com/sports'>Sports</a>
           </li>
-          <li className='menu-list4 YRN'>
-          <a href='https://giphy.com/'>Stickers</a>
+          <li className='menu-list4 TAG'>
+          <a href='https://giphy.com/stickers'>Stickers</a>
           </li>
-          <li className='menu-list5 YRN'>
-          <a href='https://giphy.com/'>Artists</a>
+          <li className='menu-list5 TAG'>
+          <a href='https://giphy.com/artists'>Artists</a>
           </li>
         </ul>
       </div>
